259fc02aa9e381cf99dc6effcc7426517c596dca
GoldenBoyPapoy <BlueGeniusDev@gmail.com>
2023-06-04 15:26:52 -0400

    add config information

--- src/components/components/categories.jsx
+++ src/components/components/categories.jsx
@@ -6,29 +6,25 @@ import Toolbar from "@mui/material/Toolbar";
 import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
 import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 import IconButton from "@mui/material/IconButton";
-
+import config from "../resource/config";
 const DirectionStack = ({ category, setCategory, setSource }) => {
   const [categories, setCategories] = useState([]);
   const visibleCount = 6;
   const totalCount = categories.length + 1;
   const [current, setCurrent] = useState(1);
-  const token =
-    "patwxGIW1IEy4obje.4698f2fae36a50dd8b00fb6335086193541405e1bf506bd86f8875db903e045d";
+  const token = config.token;
   const setCurrentPage = (page) => {
     if (page >= 1 && page <= totalCount - visibleCount + 1) setCurrent(page);
   };
   useEffect(() => {
     const fetchCategories = async () => {
       try {
-        const response = await fetch(
-          "https://api.airtable.com/v0/appCkZvSU8PE6pKwN/tblsh9FVFPqqiUhHt",
-          {
-            method: "GET",
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          }
-        );
+        const response = await fetch(`https://api.airtable.com/v0/${config}`, {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
         const data = await response.json();
         setCategories(data.records);
       } catch (error) {
---
+++ src/components/resource/config.js
@@ -0,0 +1,7 @@
+const config = {
+  token:
+    "patwxGIW1IEy4obje.4698f2fae36a50dd8b00fb6335086193541405e1bf506bd86f8875db903e045d",
+  uri: "appCkZvSU8PE6pKwN/tblsh9FVFPqqiUhHt",
+};
+
+export default config;
